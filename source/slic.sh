ed2b5a9fdba2847ba583628dafa714555785e385
Tai Warner <twarner2@cs.swarthmore.edu>
2018-04-06 13:13:29 -0400

    more pics and some tweaks

---
+++ source/slic.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+python slic.py -i input/alexgrey.jpg &
+python slic.py -i input/kandinsky1.jpg &
+python slic.py -i input/kandinsky2.jpg &
+python slic.py -i input/leafchair.jpg &
+python slic.py -i input/crouch.jpg &
+python slic.py -i input/crum.jpg &
+python slic.py -i input/badams.jpg &
+python slic.py -i input/chakras.jpg &
+python slic.py -i input/flower.jpg &
+python slic.py -i input/plants.jpg &
+python slic.py -i input/circle.jpg &
+python slic.py -i input/mother.jpg &
+python slic.py -i input/ahuizotl.jpg &
