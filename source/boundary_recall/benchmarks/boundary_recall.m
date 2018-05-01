1f3860e85536e54e408d8bf65b2ad2332aae013e
Henry <hhan3@swarthmore.edu>
2018-05-01 13:55:11 -0400

    debugging testing code

--- source/boundary_recall/benchmarks/boundary_recall.m
+++ source/boundary_recall/benchmarks/boundary_recall.m
@@ -3,7 +3,7 @@
 % INPUT
 %   imgDir: folder containing original images
 %   gtDir:  folder containing ground truth data.
-%   pbDir:  folder containing boundary detection results for all the images in imgDir. 
+%   pbDir:  folder containing boundary detection results for all the images in imgDir.
 %           Format can be one of the following:
 %             - a soft or hard boundary map in PNG format.
 %             - a collection of segmentations in a cell 'segs' stored in a mat file
@@ -12,12 +12,12 @@
 %       nthresh : Number of points in precision/recall curve.
 
 
-imgDir = "";
-gtDir = "";
-pbDir = "";
-outDir = "";
+imgDir = 'original_images/';
+gtDir = 'ground_truth/';
+pbDir = 'boundaries/';
+outDir = 'results/';
 mkdir(outDir);
-nthresh = ;
+nthresh = 5;
 
 tic;
 boundaryBench(imgDir, gtDir, pbDir, outDir, nthresh);
